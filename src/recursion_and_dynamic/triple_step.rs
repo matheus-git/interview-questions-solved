c71e9aead350cc2a7c9714c82d33795789b04b25
Matheus-git <mathiew0@gmail.com>
2025-07-30 10:22:48 -0300

    triple step

--- src/recursion_and_dynamic/triple_step.rs
+++ src/recursion_and_dynamic/triple_step.rs
@@ -1,7 +1,9 @@
 pub fn count_ways(n: usize) -> u64 {
     let mut memo: Vec<u64> = vec![u64::MAX; n + 1];
     fn count_ways_recursive(n: usize, memo: &mut Vec<u64>) -> u64 {
-        if n < 4 {
+        if n == 0 {
+            return 1;
+        } else if n < 4 {
             return n as u64;
         } else if let Some(v) = memo.get(n) {
             if *v != u64::MAX {
